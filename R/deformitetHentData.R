f97f2e763d82936744267199d1951c58d01f11de
kevinthon <kevin.thon@gmail.com>
2024-09-05 13:33:37 +0200

    funksjon for lesing fra database

---
+++ R/deformitetHentData.R
@@ -0,0 +1,19 @@
+#' Hent tabell fra Deformitets database
+#'
+#' @param tabellnavn Navn på tabell som skal lastes inn.
+#'                   Kan ha følgende verdier:
+#'                   mce
+#'                   mce_patient_data
+#'                   patient
+#'                   patientfollowup
+#'                   patientform
+#'                   surgeonfollowup
+#'                   surgeonform
+deformitetHentTabell <- function(tabellnavn = "surgeonform") {
+  registryName <- "deformitet"
+  dbType <- "mysql"
+  query <- paste0("SELECT * FROM ", tabellnavn)
+
+  tabell <- rapbase::loadRegData(registryName, query, dbType)
+  return(tabell)
+}
