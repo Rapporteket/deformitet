d6b512ee1b52e9965082d23c9cdf6bbc87b5620d
IngridBondevik <ingridbondevik@outlook.com>
2024-09-12 13:02:36 +0200

    made new script for loading and cleaning data

---
+++ doc/clean_data.R
@@ -0,0 +1,4 @@
+# ?? Hva gjør jeg med NA?
+# ?? Er det viktig at alle mceid er med i alle tabellene?
+# ?? Jeg må legge til sykehus -> centreid
+# ?? Jeg må se på datoformatet - kan jeg bruke det slik det er nå? Er det likt i alle tabellene?
