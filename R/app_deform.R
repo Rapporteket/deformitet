881857c7a39eaf18770f6c111e9428e8042a87fb
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2026-01-19 11:24:17 +0100

    Flyttet kjor_app_deform til app_deform.R (#82)
    Slettet fil hjelpeFunksjoner.R, som kun innehold denne funksjonen.

--- R/app_deform.R
+++ R/app_deform.R
@@ -242,3 +242,12 @@ server_deform <- function(input, output, session) {
 
 
 }
+
+#' Run the Deformitet Shiny Application
+#'
+#' @return An object representeing the Deformitet app
+#' @export
+
+kjor_app_deform <- function() {
+  shiny::shinyApp(ui = ui_deform, server = server_deform)
+}
--- R/hjelpeFunksjoner.R
+++
@@ -1,8 +0,0 @@
-#' Run the Deformitet Shiny Application
-#'
-#' @return An object representeing the Deformitet app
-#' @export
-
-kjor_app_deform <- function() {
-  shiny::shinyApp(ui = ui_deform, server = server_deform)
-}
