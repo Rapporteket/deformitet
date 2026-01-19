87f04e3731dd1c79b0f4031047d8fc0fce88bd47
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-01-19 12:33:09 +0100

    Dokumenter skjema-data
    Flyttet også kode som produserer data til mappen data-raw.
    
    Closes #80

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' A dataset containing an overview of variables in the scheme.
+#'
+#' @format ## `skjema`
+#' A data frame with 18 rows and 8 variables:
+#' \describe{
+#'   \item{pre_op_pas}{Not used}
+#'   \item{pre_op_lege}{Not used}
+#'   \item{tre_mnd_pas}{Used in the prepvar function}
+#'   \item{tre_mnd_lege}{Used in the prepvar function}
+#'   \item{tolv_mnd_pas}{Used in the prepvar function}
+#'   \item{tolv_mnd_lege}{Used in the prepvar function}
+#'   \item{seksti_mnd_pas}{Used in the prepvar function}
+#'   \item{andre}{Not used}
+#' }
+#'
+"skjema"
